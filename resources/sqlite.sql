68c038a1c6049ebc3195f78a6551d62f505c52b6
UramnOIL <uramnoil@outlook.com>
2018-10-06 22:30:03 +0900

    getRankingByKill, getRankingByExpの追加

--- resources/sqlite.sql
+++ resources/sqlite.sql
@@ -1,46 +1,89 @@
--- #! sqlite
--- #{ account
--- #  { init
+-- #! !sqlite
+-- #  {userdataprovider
+
+--Accounts
+
+-- #  { accounts
+
+-- #    { init
 CREATE TABLE IF NOT EXISTS accounts(
   id   INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL
 );
--- #  }
--- #  { register
--- #    :name string
+-- #    }
+
+-- #    { register
+-- #      :name string
 INSERT INTO accounts(
   name
 ) VALUES (
   :name
 );
+-- #    }
+
+-- #    { unregister
+-- #      :id int
+DELETE FROM accounts
+WHERE id = :id;
+-- #    }
 -- #  }
--- #}
--- #{ ffapvp
--- #  { init
+
+--FFAPvP
+
+-- #  { ffapvp
+-- #    { init
 CREATE TABLE IF NOT EXISTS ffapvp(
   id INTEGER NOT NULL,
   kill INTEGER NOT NULL DEFAULT 0,
   death INTEGER NOT NULL DEFAULT 0,
   exp INTEGER NOT NULL DEFAULT 0
 );
--- #  }
--- #  { register
--- #    :id int
+-- #    }
+
+-- #    { register
+-- #      :id int
 INSERT INTO ffapvp(
   id
 ) VALUES (
   :id
 );
--- #  }
--- #  { addCount
--- #    :kill int
--- #    :death int
--- #    :exp int
+-- #    }
+
+-- #    { unregister
+-- #      :id int
+DELETE FROM ffapvp
+WHERE id = :id;
+-- #    }
+
+-- #    { addCount
+-- #      :kill int
+-- #      :death int
+-- #      :exp int
 UPDATE ffapvp
 SET kill = kill + :kill,
     death = death + :death,
-    exp = exp + :exp
--- #  }
--- #  {
+    exp = exp + :exp;
+-- #    }
+
+-- #    { getRankingByExp
+-- #      :limit int
+SELECT accounts.name, accounts.id, ffapvp.kill, ffapvp.death, ffapvp.exp
+FROM ffapvp
+INNER JOIN accounts
+ON ffapvp.id = accounts.id
+LIMIT :limit
+ORDER BY exp DESC;
+-- #    }
+
+-- #    { getRankingByKill
+-- #      :limit int
+SELECT accounts.name, accounts.id, ffapvp.kill, ffapvp.death, ffapvp.exp
+FROM ffapvp
+INNER JOIN accounts
+ON ffapvp.id = accounts.id
+limit :limit
+ORDER BY kill ASC
+-- #    }
+
 -- #  }
 -- #}
