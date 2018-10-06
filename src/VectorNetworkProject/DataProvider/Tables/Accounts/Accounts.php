eff4c4850c51dd2a0cc75c5a04ec9dcfbec7928e
UramnOIL <uramnoil@outlook.com>
2018-10-06 23:09:45 +0900

    Queriesの作成、それに伴う変更

--- src/VectorNetworkProject/DataProvider/Tables/Accounts/Accounts.php
+++ src/VectorNetworkProject/DataProvider/Tables/Accounts/Accounts.php
@@ -9,26 +9,22 @@
 namespace VectorNetworkProject\DataProvider\Tables\Accounts;
 
 use pocketmine\IPlayer;
+use VectorNetworkProject\DataProvider\Tables\Queries;
 use VectorNetworkProject\DataProvider\Tables\TableBase;
 
 class Accounts extends TableBase
 {
-	public const INIT = 'userdataprovider.accounts.register';
-	public const REGISTER = 'userdataprovider.accounts.unregister';
-	public const UNREGISTER = 'userdataprovider.accounts.unregister';
-	public const GET = 'userdataprovider.accounts.get';
-
 	public function register(IPlayer $player, ?callable $handler = null, ?callable $errorHandler = null)
 	{
-		$this->connector->executeInsert(self::REGISTER, [$player->getName()], $handler, $errorHandler);
+		$this->connector->executeInsert(Queries::ACCOUNT_REGISTER, [$player->getName()], $handler, $errorHandler);
 	}
 
 	public function unregister(IPlayer $player, ?callable $handler = null, ?callable $errorHandler = null)
 	{
-		$this->connector->executeChange(self::UNREGISTER, [$player->getName()], $handler, $errorHandler);
+		$this->connector->executeChange(Queries::ACCOUNT_UNREGISTER, [$player->getName()], $handler, $errorHandler);
 	}
 
 	public function get(IPlayer $player, ?callable $handler = null, ?callable $errorHandler = null){
-		$this->connector->executeSelect(self::GET, [$player->getName()], $handler, $errorHandler);
+		$this->connector->executeSelect(Queries::ACCOUNT_GET, [$player->getName()], $handler, $errorHandler);
 	}
 }
---
+++ src/VectorNetworkProject/DataProvider/Tables/Queries.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: UramnOIL
+ * Date: 2018/10/06
+ * Time: 23:07
+ */
+
+namespace VectorNetworkProject\DataProvider\Tables;
+
+
+class Queries
+{
+	public const ACCOUNT_INIT = 'userdataprovider.accounts.register';
+	public const ACCOUNT_REGISTER = 'userdataprovider.accounts.unregister';
+	public const ACCOUNT_UNREGISTER = 'userdataprovider.accounts.unregister';
+	public const ACCOUNT_GET = 'userdataprovider.accounts.get';
+}
