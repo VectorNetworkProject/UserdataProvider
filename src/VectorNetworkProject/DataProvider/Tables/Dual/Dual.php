ca62755272c4c431c29921d7b5e4268404c0a09e
UramnOIL <uramnoil@outlook.com>
2018-10-07 00:51:05 +0900

    Dualクラスの作成

---
+++ src/VectorNetworkProject/DataProvider/Tables/Dual/Dual.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: UramnOIL
+ * Date: 2018/10/07
+ * Time: 0:50
+ */
+
+namespace VectorNetworkProject\DataProvider\Tables\Dual;
+
+
+use VectorNetworkProject\DataProvider\Tables\TableBase;
+
+class Dual extends TableBase
+{
+	public function init(): void
+	{
+		// TODO: Implement init() method.
+	}
+}
