c2b2a1b2dc19c27bb6081db8432e6d4b938ff2dc
UramnOIL <uramnoil@outlook.com>
2018-10-07 09:16:12 +0900

    コメントの変更

--- src/VectorNetworkProject/DataProvider/Tables/Accounts/Accounts.php
+++ src/VectorNetworkProject/DataProvider/Tables/Accounts/Accounts.php
@@ -23,17 +23,38 @@ class Accounts extends TableBase
 		$this->connector->executeGeneric(self::ACCOUNT_INIT);
 	}
 
-	public function register(IPlayer $player, ?callable $handler = null, ?callable $errorHandler = null)
+	/**
+	 * プレイヤーを登録します
+	 *
+	 * @param IPlayer $player
+	 * @param callable|null $onInserted
+	 * @param callable|null $onError
+	 */
+	public function register(IPlayer $player, ?callable $onInserted = null, ?callable $onError = null)
 	{
-		$this->connector->executeInsert(self::ACCOUNT_REGISTER, [$player->getName()], $handler, $errorHandler);
+		$this->connector->executeInsert(self::ACCOUNT_REGISTER, [$player->getName()], $onInserted, $onError);
 	}
 
-	public function unregister(IPlayer $player, ?callable $handler = null, ?callable $errorHandler = null)
+	/**
+	 * プレイヤーを登録解除します
+	 *
+	 * @param IPlayer $player
+	 * @param callable|null $onSuccess
+	 * @param callable|null $onError
+	 */
+	public function unregister(IPlayer $player, ?callable $onSuccess = null, ?callable $onError = null)
 	{
-		$this->connector->executeChange(self::ACCOUNT_UNREGISTER, [$player->getName()], $handler, $errorHandler);
+		$this->connector->executeChange(self::ACCOUNT_UNREGISTER, [$player->getName()], $onSuccess, $onError);
 	}
 
-	public function get(IPlayer $player, ?callable $handler = null, ?callable $errorHandler = null){
-		$this->connector->executeSelect(self::ACCOUNT_GET, [$player->getName()], $handler, $errorHandler);
+	/**
+	 * プレイヤーの情報を取得します
+	 *
+	 * @param IPlayer $player
+	 * @param callable|null $onSelect
+	 * @param callable|null $onError
+	 */
+	public function get(IPlayer $player, ?callable $onSelect = null, ?callable $onError = null){
+		$this->connector->executeSelect(self::ACCOUNT_GET, [$player->getName()], $onSelect, $onError);
 	}
 }
