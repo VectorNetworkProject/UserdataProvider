603e82236a260d69ec00934db9cdf0c0cb963c3d
UramnOIL <uramnoil@outlook.com>
2018-10-07 09:22:56 +0900

    onDisable()の処理の追加

--- src/VectorNetworkProject/DataProvider/Main.php
+++ src/VectorNetworkProject/DataProvider/Main.php
@@ -11,6 +11,7 @@ namespace VectorNetworkProject\DataProvider;
 
 use pocketmine\plugin\PluginBase;
 use poggit\libasynql\libasynql;
+use poggit\libasynql\DataConnector;
 use VectorNetworkProject\DataProvider\Tables\TableManager;
 
 class Main extends PluginBase
@@ -18,13 +19,23 @@ class Main extends PluginBase
 
 	/** @var TableManager */
 	private $manager;
+	/** @var DataConnector */
+	private $connector;
 	public function onEnable()
 	{
 		$this->saveDefaultConfig();
-		$database = libasynql::create($this, $this->getConfig()->get("database"), [
+		$this->connector = $connector = libasynql::create($this, $this->getConfig()->get("database"), [
 			"sqlite" => "sqlite.sql",
 			"mysql" => "mysql.sql"
 		]);
-		$this->manager = new TableManager($database);
+		$this->manager = new TableManager($connector);
+	}
+
+	public function onDisable()
+	{
+		if($this->connector !== null)
+		{
+			$this->connector->close();
+		}
 	}
 }
