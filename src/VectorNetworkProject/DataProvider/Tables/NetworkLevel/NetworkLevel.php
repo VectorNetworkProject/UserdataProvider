8ea2959ff8401a1f7d27e13c174949a73774e957
UramnOIL <uramnoil@outlook.com>
2018-10-13 23:26:56 +0900

    NetworkLevelの作成

---
+++ src/VectorNetworkProject/DataProvider/Tables/NetworkLevel/NetworkLevel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: UramnOIL
+ * Date: 2018/10/13
+ * Time: 23:26
+ */
+
+namespace VectorNetworkProject\DataProvider\Tables\NetworkLevel;
+
+
+class NetworkLevel
+{
+
+}
