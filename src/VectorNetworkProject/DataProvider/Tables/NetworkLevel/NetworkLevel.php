0cc3b8ddae016fca1d8ec2ed4ef764ce60c81dd0
UramnOIL <uramnoil@outlook.com>
2018-10-13 23:32:40 +0900

    関数の作成

--- src/VectorNetworkProject/DataProvider/Tables/NetworkLevel/NetworkLevel.php
+++ src/VectorNetworkProject/DataProvider/Tables/NetworkLevel/NetworkLevel.php
@@ -8,8 +8,66 @@
 
 namespace VectorNetworkProject\DataProvider\Tables\NetworkLevel;
 
+use pocketmine\IPlayer;
+use VectorNetworkProject\DataProvider\Tables\TableBase;
 
-class NetworkLevel
+class NetworkLevel extends TableBase
 {
+	public const INiT = 'databaseprovider.networklevel.init';
+	public const REGISTER = 'databaseprovider.networklevel.register';
+	public const UNREGISTER = 'databaseprovider.networklevel.unregister';
+	public const GET = 'databaseprovider.networklevel.get';
+	public const ADD = 'databaseprovider.networklevel.add';
 
+	public function init(): void
+	{
+		$this->connector->executeGeneric(self::INiT);
+	}
+
+	/**
+	 * プレイヤーを登録します。
+	 *
+	 * @param IPlayer $player
+	 * @param callable|null $onInserted
+	 * @param callable|null $onError
+	 */
+	public function register(IPLayer $player, ?callable $onInserted = null, ?callable $onError = null): void
+	{
+		$this->connector->executeInsert(self::REGISTER, [$player->getname()], $onInserted, $onError);
+
+	}
+
+	/**
+	 * プレイヤーを登録解除します
+	 *
+	 * @param IPlayer $player
+	 * @param callable|null $onSuccess
+	 * @param callable|null $onError
+	 */
+	public function unregister(IPLayer $player, ?callable $onSuccess = null, ?callable $onError = null): void
+	{
+		$this->connector->executeChange(self::UNREGISTER, [$player->getName()], $onSuccess, $onError);
+	}
+
+	/**
+	 * プレイヤーの情報を取得します
+	 *
+	 * @param IPlayer $player
+	 * @param callable|null $onSuccess
+	 * @param callable|null $onError
+	 */
+	public function get(IPlayer $player, callable $onSuccess = null, ?callable $onError = null): void
+	{
+		$this->connector->executeSelect(self::GET, [$player->getName()], $onSuccess, $onError);
+	}
+
+	/**
+	 * プレイヤーのそれぞれのカウントを増やします
+	 *
+	 * @param IPlayer $player
+	 * @param int $exp
+	 */
+	public function add(IPlayer $player, int $exp = 0) {
+		$this->connector->executeChange(self::ADD, [$player->getName(), $exp]);
+	}
 }
